b367372dbec9a45262a3e8355ec095928b1c8042
MRATTE <105413905+matelec@users.noreply.github.com>
2023-03-11 08:44:39 +0100

    Update exemple_I2C_01.py

--- docs/code/exemple_I2C_01.py
+++ docs/code/exemple_I2C_01.py
@@ -1,11 +1,12 @@
 from machine import Pin, I2C
 import time
 
-#configuration du bus I2C sur l'ESP8266
+#configuration du bus I2C sur l'ESP8266.
+#le timeout est nécessaire pour eviter l'erreur watch dog si pas de réponse sur le Bus.
 i2c = I2C(scl=Pin(5), sda=Pin(4), freq=100000, timeout=5000)
 
 print ("san du bus i2c")
-devices = i2c.scan()
+devices = i2c.scan()    #rangement des adresses des périphériques dans une list.
 
 if len(devices) == 0:
     print("aucun périphérique sur le bus I2C")
